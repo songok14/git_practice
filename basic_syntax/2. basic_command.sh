0f155a3f489220652b6b57b78dd5f89f5ec06849
songok14 <rlathddhr11@gmail.com>
2025-06-04 11:29:41 +0900

    test

--- basic_syntax/2. basic_command.sh
+++ basic_syntax/2. basic_command.sh
@@ -16,8 +16,18 @@ git commit
 # commit 이력 확인(버전 확인)
 git log
 git log --oneline
+# head 하단의 로그만 보이는 것이 아니라 모든 log이력을 조회
+# head: 현재 checkout 하고 있는 commitID를 의미
+git log --all
 
 # 원격저장소로 업로드
 git push origin 브랜치명
 # 중복발생 시 충돌 무시하고 로컬 기준으로 원격에 덮어쓰기
 git push origin 브랜치명 --force
+
+# 특정 commit ID로의 전환
+git checkout 커밋ID
+# 특정 브랜치로의 전환환
+git checkout 브랜치명
+
+# 
