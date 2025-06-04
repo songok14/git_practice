14574e00954ff61bd78543105490f48e853178f1
songok14 <rlathddhr11@gmail.com>
2025-06-04 10:32:54 +0900

    version1

--- basic_syntax/2. basic_command.sh
+++ basic_syntax/2. basic_command.sh
@@ -12,3 +12,12 @@ git commit -m "메시지 제목" -m "메시지 내용"
 
 # git commit만 입력하고 엔터 시 vi 모드 -> 첫줄 타이틀, 두번째 줄 내용
 git commit
+
+# commit 이력 확인(버전 확인)
+git log
+git log --oneline
+
+# 원격저장소로 업로드
+git push origin 브랜치명
+# 중복발생 시 충돌 무시하고 로컬 기준으로 원격에 덮어쓰기
+git push origin 브랜치명 --force
